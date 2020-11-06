3cfcb7f8c1702b845a7c9673f8c2dcc66fa76d83
de_gentlem4n <th3g3n3lm4n@gmail.com>
2020-11-06 14:05:20 -0500

    life is slow still working...sad

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import Characters from './components/Character';
+import Axios from 'axios';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  const [cList, setCList] = useState([])
+  
+  useEffect(() => {
+    Axios.get('https://rickandmortyapi.com/api/character/')
+      .then(res =>{
+        console.log(res.data.results)
+        setCList(res.data.results)
+      })
+      .catch(err => console.log(err))
+  },[])
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Rick & Morty API</h1>
+      <Characters characters={cList} />
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,28 @@
-// Write your Character component here
+import React,{useState} from 'react';
+
+export default function Character(props){
+    const characters = props.characters;
+
+    const [mainCharacter , setMainCharacter] = useState({})
+
+    
+
+    return (
+        <div>
+            test
+            <CardBuilder characters={characters} />
+        </div>
+    )
+}
+
+
+function CardBuilder(props){
+
+
+
+    return(
+        <div>
+            poo
+        </div>
+    )
+}
